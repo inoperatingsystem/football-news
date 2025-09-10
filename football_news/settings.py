68620993abb10c066bbeca2d5c8eec897d1e00ae
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-09-10 16:49:53 +0700

    fix: add https not http for pws deplyoment in CSRF_TRUSTED_ORIGINS

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "julius-albert-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://julius-albert-footballnews.pbp.cs.ui.ac.id"
+    "https://julius-albert-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
